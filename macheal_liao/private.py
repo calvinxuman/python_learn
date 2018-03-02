c171bc3135d6bb6718df36cc52285df9dd434080
xuman <13207140256@139.com>
2018-03-02 17:08:48 +0800

    property

---
+++ macheal_liao/private.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/1  14:57
+# @Author  : calvin
+
+class Student():
+    def __init__(self,name,gender):
+        self.__name = name
+        self.__gender = gender
+
+    def set_gender(self,name):
+        if name == 'female':
+            self.__gender = 'female'
+        elif name == 'male':
+            self.__gender = 'male'
+        else:
+            print('Invalid gender!')
+
+    def get_name(self):
+        return self.__name
+
+    def get_gender(self):
+        return self.__gender
+
+bart = Student('Bart', 'male')
+print(bart.get_gender())
+bart.set_gender('female')
+print(bart.get_gender())
+bart.set_gender('null')
+print(bart.get_name())
+
--- macheal_liao/测试.py
+++ macheal_liao/测试.py
@@ -1,14 +1,50 @@
 # -*- coding: utf-8 -*-
 
-def is_odd(n):
-    return n % 2 == 1
+#给一个Screen对象加上width和height属性，以及一个只读属性resolution
+class Screen():
+    #__slots__ = ('width','height','resolution')
 
-L = list(filter(is_odd, range(1, 20)))
+    # def __init__(self,width,height):
+    #     self.__width = width
+    #     self.__height = height
 
-L2= list(filter(lambda x:x % 2 ==1,range(1, 20)))
+    @property
+    def width(self):
+        return self.__width
 
-print(L)
-print(L2)
+    @width.setter
+    def width(self,width):
+        if not isinstance(width,int):
+            raise ValueError('width must be an integer!')
+        if width < 0 or width > 1920:
+            raise ValueError('width must between 0 ~ 1920!')
+        else:
+            self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self,height):
+        if not isinstance(height,int):
+            raise ValueError('height must be an integer!')
+        if height < 0 or height > 1080:
+            raise ValueError('height must between 0 ~ 1080!')
+        else:
+            self.__height = height
+
+    @property
+    def resolution(self):
+        return self.__width * self.__height
+
+s = Screen()
+s.width = 1920
+s.height = 1080
+print('resolution =', s.resolution)
+
+
+
 
 
 
